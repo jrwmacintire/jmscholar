2408751cfa3168c83fea54908e74c18026f07f91
John MacIntire <jrwmacintire@gmail.com>
2019-01-05 22:35:44 -0700

    Removed EJS and added Pug to the Express templating engine.

--- app.js
+++ app.js
@@ -39,8 +39,9 @@ if (cluster.isMaster) {
     var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
     var app = express();
 
-    app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, '/views'));
+    app.set('view engine', 'pug');
+    app.set('views', './public/views');
+    app.use(express.static(__dirname + '/public'));
     app.use(bodyParser.urlencoded({extended:false}));
 
     app.get('/', function(req, res) {
